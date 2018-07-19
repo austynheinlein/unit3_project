aab4fc872df830b3f60cf2246393ea41ca94ddf4
your name <you@example.com>
2018-07-19 16:36:37 -0700

    cleaning up master

--- server.js
+++ server.js
@@ -4,29 +4,6 @@ const mongoose = require('mongoose')
 const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/rentr_app';
  mongoose.connect(mongoUri);
 
- // JSON middleware
- app.use(express.json());
-
- // Public middleware
- app.use(express.static("public"));
-
-
- // Session middleware
- app.use(session({
-     secret:'feedmeseymour',
-     resave: false,
-     saveUninitialized: false
- }));
-
- /// req.body middleware
- app.use(express.urlencoded({extended:false}));
-
- // controllers
-
- // properties controller
-
- const propertiesController = require('./controllers/properties.js');
- app.use('/properties', propertiesController);
 
  const port = process.env.PORT || 3000;
  app.listen(port);
