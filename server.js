fb37bac25baec9a20d09d7cac873f540392d2265
Joseph Weber <joseph.theodore.weber@gmail.com>
2018-07-19 18:27:43 -0400

    added bcryptjs

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require('mongoose')
 const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/rentr_app';
  mongoose.connect(mongoUri);
+ const session = require('express-session');
+ const bcryptjs = require('bcryptjs')
 
  // JSON middleware
  app.use(express.json());
