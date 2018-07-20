c8c1414fa16cd5d5c4bf89a078f09b8526546223
acroak <38078917+acroak@users.noreply.github.com>
2018-07-20 16:14:10 -0400

    Revert "Partials"

--- server.js
+++ server.js
@@ -3,40 +3,7 @@ const app = express();
 const mongoose = require('mongoose')
 const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/rentr_app';
  mongoose.connect(mongoUri);
- const session = require('express-session');
- const bcryptjs = require('bcryptjs')
 
-
- // JSON middleware
- app.use(express.json());
-
- // Public middleware
- app.use(express.static("public"));
-
-
- // Session middleware
- app.use(session({
-     secret:'feedmeseymour',
-     resave: false,
-     saveUninitialized: false
- }));
-
- /// req.body middleware
- app.use(express.urlencoded({extended:false}));
-
- // controllers
-
- // properties controller
- const propertiesController = require('./controllers/properties.js');
- app.use('/properties', propertiesController);
-
- //users controller
- const usersController = require('./controllers/users.js');
- app.use('/users', usersController);
-
- //sessions controller
- const sessionsController = require('./controllers/sessions.js');
- app.use('/sessions', sessionsController);
 
  const port = process.env.PORT || 3000;
  app.listen(port);
