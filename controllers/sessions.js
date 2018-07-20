7a9ca2bf7c7ab1cd7c859a5d619fe85c7ac30b38
acroak <acroak@live.com>
2018-07-20 10:36:24 -0400

    switching from bcrypt to bcrypt js in sessions controller

---
+++ controllers/sessions.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/users.js');
+const bcrypt = require('bcryptjs');
+
+//Login /POST
+router.post('/',(req,res)=>{
+  console.log('logging in...');
+  User.findOne({username:req.body.username}, (err, foundUser)=>{
+    if(bcrypt.compareSync(req.body.password, foundUser.password)){
+      req.session.currentUser = foundUser;
+      res.status(201).json({
+        status:201,
+        message: 'session created'
+      });
+    } else {
+      res.status(401).json({
+        status:401,
+        message:'Login failed'
+      });
+    }
+  })
+})
+
+
+//Logout / DELETE
+router.delete('/', (req,res)=>{
+  console.log('logging out...');
+  req.session.destroy(()=>{
+    res.status(200).json({
+      status: 200,
+      message: 'Logout complete'
+    });
+  })
+});
+
+module.exports = router;
