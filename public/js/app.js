c9881c282296e2f177015c520eabac46f5aa1f81
acroak <acroak@live.com>
2018-07-20 11:31:34 -0400

    created get route for properties in app.js and added call components to index.html, displaying property data!

--- public/js/app.js
+++ public/js/app.js
@@ -4,4 +4,18 @@ const app = angular.module('MyApp', []);
 app.controller('MyController', ['$http', function($http){
   const controller = this;
   this.funny = 'haha';
+
+//GET
+  this.getProperties = function(){
+    $http({
+      method:'GET',
+      url: '/properties'
+    }).then(function(response){
+      controller.properties = response.data
+    })
+  }
+
+
+
+  this.getProperties();
 }]);
